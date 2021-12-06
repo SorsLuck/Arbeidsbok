2a2aed2d9f1f4feba4daaed5e0471e80a34d3d31
Sors <46603450+SorsLuck@users.noreply.github.com>
2021-12-06 10:59:56 +0100

    Add files via upload

---
+++ JS/Konami_code.js
@@ -0,0 +1,14 @@
+function onKonamiCode(cb) {
+    var input = '';
+    var key = '38384040373937396665';
+    document.addEventListener('keydown', function (e) {
+      input += ("" + e.keyCode);
+      if (input === key) {
+        return cb();
+      }
+      if (!key.indexOf(input)) return;
+      input = ("" + e.keyCode);
+    });
+  }
+  
+  onKonamiCode(function () {window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')})
